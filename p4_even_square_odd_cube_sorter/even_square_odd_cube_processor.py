42adb0854b7bf0057a95c3cf84b3ddb8f9b3e113
Kryzle <kryzlesayat@gmail.com>
2026-04-29 01:19:13 +0800

    added modulo logic and calculation for square and cube

--- p4_even_square_odd_cube_sorter/even_square_odd_cube_processor.py
+++ p4_even_square_odd_cube_sorter/even_square_odd_cube_processor.py
@@ -9,6 +9,10 @@ class PowerProcessor:
         with open(self.filename, 'r') as file:
             for integers in file:
                 integer = int(integers.strip())
+                if integer % 2 == 0:
+                    self.squares.append(integer ** 2)
+                else:
+                    self.cubes.append(integer ** 3)
 
     def save_results(self):
         """Writes the transformed results into double.txt and triple.txt."""
