78c00959bc32cf4ab248888adba09068540c54a2
Kryzle <kryzlesayat@gmail.com>
2026-04-29 00:16:18 +0800

    added ANSI color constants

--- p3_write_multiple_line_content/line_writer.py
+++ p3_write_multiple_line_content/line_writer.py
@@ -1,3 +1,7 @@
 class LifeRecorder:
     def __init__(self, filename="mylife.txt"):
         self.filename = filename
+        self.purple = "\033[95m"
+        self.cyan = "\033[96m"
+        self.yellow = "\033[93m"
+        self.reset = "\033[0m"
