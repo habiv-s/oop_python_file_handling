8bc8112bf9a887cdc854e26684e48f0c50893518
Kryzle <kryzlesayat@gmail.com>
2026-04-29 02:23:06 +0800

    updated runner script for the report

--- p4_even_square_odd_cube_sorter/even_square_odd_cube_runner_script.py
+++ p4_even_square_odd_cube_sorter/even_square_odd_cube_runner_script.py
@@ -3,3 +3,4 @@ from even_square_odd_cube_processor import PowerProcessor
 processor = PowerProcessor("integers.txt")
 processor.process_numbers()
 processor.save_results()
+processor.generate_report()
