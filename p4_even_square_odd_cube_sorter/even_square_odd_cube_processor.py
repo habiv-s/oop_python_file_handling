615d324546d124cbe186ac8a1939979fff5e0c39
Kryzle <kryzlesayat@gmail.com>
2026-04-29 01:59:19 +0800

    added logic for computing the averages

--- p4_even_square_odd_cube_sorter/even_square_odd_cube_processor.py
+++ p4_even_square_odd_cube_sorter/even_square_odd_cube_processor.py
@@ -32,6 +32,15 @@ class PowerProcessor:
         """Calculates counts and averages."""
         squares_count = len(self.squares)
         cubes_count = len(self.cubes)
+        if squares_count > 0:
+            squares_average = sum(self.squares) / squares_count
+        else:
+            squares_average = 0
+        if cubes_count > 0:
+            cubes_average = sum(self.cubes) / cubes_count
+        else:
+            cubes_average = 0
+        return squares_count, cubes_count, squares_average, cubes_average
 
     def get_report_template(self, square_count, cube_count, square_average, cube_average):
         """Defines the visual style of the report."""
