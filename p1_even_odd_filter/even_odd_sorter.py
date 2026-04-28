2c28ea7af4b673f96b4f7fedeb845c3423317cf0
Kryzle <kryzlesayat@gmail.com>
2026-04-28 22:09:08 +0800

    fixed indentation

--- p1_even_odd_filter/even_odd_sorter.py
+++ p1_even_odd_filter/even_odd_sorter.py
@@ -4,26 +4,26 @@ class NumberSorter:
         self.even_list = []
         self.odd_list = []
 
-def sort_numbers(self):
-    """Reads integers and categorizes them into even or odd lists."""
-    try:
-        with open('numbers.txt', 'r') as numbers_file:
-            for numbers in numbers_file:
-                number = int(numbers.strip())
-                if number % 2 == 0:
-                    self.even_list.append(number)
-                else:
-                    self.odd_list.append(number)
-        print(f"Successfully processed numbers from {self.numbers}.")
-    except FileNotFoundError:
-        print("Sorry, the numbers text file was not found.")
+    def sort_numbers(self):
+        """Reads integers and categorizes them into even or odd lists."""
+        try:
+            with open('numbers.txt', 'r') as numbers_file:
+                for numbers in numbers_file:
+                    number = int(numbers.strip())
+                    if number % 2 == 0:
+                        self.even_list.append(number)
+                    else:
+                        self.odd_list.append(number)
+            print(f"Successfully processed numbers from {self.numbers}.")
+        except FileNotFoundError:
+            print("Sorry, the numbers text file was not found.")
 
-def save_to_files(self):
-    """Creates even.txt and odd.txt from the sorted lists."""
-    with open('even.txt', 'w') as even_numbers_file:
-        for numbers in self.even_list:
-            even_numbers_file.write(f"{numbers}\n")
-    with open('odd.txt', 'w') as odd_numbers_file:
-        for numbers in self.odd_list:
-            odd_numbers_file.write(f"{numbers}\n")
-    print("Generated 'even.txt' and 'odd.txt' successfully.")
+    def save_to_files(self):
+        """Creates even.txt and odd.txt from the sorted lists."""
+        with open('even.txt', 'w') as even_numbers_file:
+            for numbers in self.even_list:
+                even_numbers_file.write(f"{numbers}\n")
+        with open('odd.txt', 'w') as odd_numbers_file:
+            for numbers in self.odd_list:
+                odd_numbers_file.write(f"{numbers}\n")
+        print("Generated 'even.txt' and 'odd.txt' successfully.")
