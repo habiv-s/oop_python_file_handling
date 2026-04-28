48a105a1caa69328fad96d237e67c6bcf676c3fd
Kryzle <kryzlesayat@gmail.com>
2026-04-29 03:48:29 +0800

    updated runner script file

--- p2_top_student_identifier_with_gwa/gwa_analyzer_runner_script.py
+++ p2_top_student_identifier_with_gwa/gwa_analyzer_runner_script.py
@@ -3,3 +3,5 @@ from gwa_analyzer import GwaAnalyzer
 analyzer = GwaAnalyzer("students_and_gwa.txt")
 analyzer.find_top_student()
 analyzer.display_result()
+analyzer.issue_digital_certificates()
+analyzer.generate_honors_report()
