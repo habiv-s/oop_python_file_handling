ad363e383e72d855c5a2d72e4091e47594d3a515
Kryzle <kryzlesayat@gmail.com>
2026-04-28 23:26:40 +0800

    fixed variable name

--- p2_top_student_identifier_with_gwa/gwa_analyzer.py
+++ p2_top_student_identifier_with_gwa/gwa_analyzer.py
@@ -10,7 +10,7 @@ class GwaAnalyzer:
             for line in file:
                 student_name, student_gwa = line.strip().split(',')
                 student_gwa = float(student_gwa)
-                if self.top_student is None or gwa < self.highest_gwa:
+                if self.top_student is None or student_gwa < self.highest_gwa:
                     self.highest_gwa = student_gwa
                     self.top_student = student_name
 
