a4a0b7fbd1b2ca72aca6aac35420e65baf3ef546
Kryzle <kryzlesayat@gmail.com>
2026-04-28 23:32:00 +0800

    added try-except for error handling

--- p2_top_student_identifier_with_gwa/gwa_analyzer.py
+++ p2_top_student_identifier_with_gwa/gwa_analyzer.py
@@ -6,14 +6,17 @@ class GwaAnalyzer:
 
     def find_top_student(self):
         """Logic to read file and find the highest GWA."""
-        with open(self.filename, 'r') as file:
-            for line in file:
-                student_name, student_gwa = line.strip().split(',')
-                student_gwa = float(student_gwa)
-                if self.top_student is None or student_gwa < self.highest_gwa:
-                    self.highest_gwa = student_gwa
-                    self.top_student = student_name
-        print("Successfully processed student records.")
+        try:
+            with open(self.filename, 'r') as file:
+                for line in file:
+                    student_name, student_gwa = line.strip().split(',')
+                    student_gwa = float(student_gwa)
+                    if self.top_student is None or student_gwa < self.highest_gwa:
+                        self.highest_gwa = student_gwa
+                        self.top_student = student_name
+            print("Successfully processed student records.")
+        except FileNotFoundError:
+            print("Error: File not found.")
 
     def display_result(self):
         """Logic to print the top student's info."""
